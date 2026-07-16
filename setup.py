d509e99a54bf3fe92e244596a80f3ccd78ee4977
MK2112 <29037335+MK2112@users.noreply.github.com>
2026-07-16 16:28:31 +0200

    make setup.py properly hook into console for install

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=[
-        "python-gnupg>=0.5.0",
+        "gnupg>=2.3.1",
         "watchdog>=2.1.9",
-        "pyyaml>=6.0",
-        "requests>=2.28.1",
-        "msal>=1.20.0",
         "cryptography>=41.0.0",
         "liboqs-python>=0.8.0",
     ],
@@ -22,5 +19,5 @@ setup(
     author_email="mk2112@protonmail.com",
     description="A middleware for PGP encryption of cloud synced files",
     keywords="sync, pgp, encryption, security, cloud, guardian-sync",
-    python_requires=">=3.6",
-) 
+    python_requires=">=3.10",
+)
